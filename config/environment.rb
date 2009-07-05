5bf3677ba646f804fe31fd49ba3e202edda07cb0
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-07-05 22:39:18 +0200

    added compass

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'haml'
+  config.gem 'haml-edge', :lib => 'haml'
   config.gem 'authlogic'
   config.gem 'state_machine'
   config.gem "mbleigh-acts-as-taggable-on", :lib => "acts-as-taggable-on"
