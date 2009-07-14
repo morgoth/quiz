0e526bb26188efcad28774b5ad0073993aa320a3
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-07-14 22:16:31 +0200

    more styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   include TagsHelper
+
+  def flash_messages
+    return if flash.empty?
+    messages = ''
+    flash.each do |key, msg|
+      messages <<  content_tag( :div, msg, :class => key)
+    end
+    messages
+  end
 end
