fc238df1d5c6ec837173e565ec4ded8503a92e9b
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-07-15 20:23:06 +0200

    sidebar partials, active link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
     end
     messages
   end
+
+  def menu_link_to(controller_name, name, options = {}, html_options = {})
+    controller_name = Array(controller_name)
+    classes = (html_options[:class] || "").split(" ")
+    classes << "active" if controller_name.include?(controller.controller_name)
+    html_options[:class] = classes.join(" ")
+    link_to(name, options, html_options)
+  end
 end
