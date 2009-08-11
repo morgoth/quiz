75abbf619737e1f15c8f11a7e8b09f9c5b76c14b
Wojciech Wnętrzak <wojtek@synergypeople.net>
2009-08-11 11:07:40 +0200

    fixed compass configuration

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem "chriseppstein-compass", :lib => "compass"
   config.gem 'haml'
   config.gem 'authlogic'
   config.gem 'state_machine'
--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,12 @@
-require 'compass'
-# If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
-Compass.configuration.environment = RAILS_ENV.to_sym
+Compass.configuration do |config|
+  config.project_type = :rails
+  config.project_path = RAILS_ROOT if defined?(RAILS_ROOT)
+  config.css_dir = "public/stylesheets/compiled"
+  config.sass_dir = "app/stylesheets"
+  config.images_dir = "public/images"
+  config.javascripts_dir = "public/javascripts"
+  config.output_style = :compact
+  config.http_images_path = "/images"
+  config.environment = RAILS_ENV.to_sym
+end
 Compass.configure_sass_plugin!
