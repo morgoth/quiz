4f1075abcf4ca900f2a2c0206f13c7464363d409
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-11-20 20:28:55 +0100

    added new formtastic sass

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,15 +7,17 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = t("controllers.notice.user_session_created")
-      redirect_back_or_default account_path
+      redirect_to root_path
+      # redirect_back_or_default account_path
     else
-      render :action => :new
+      render :new
     end
   end
 
   def destroy
     current_user_session.destroy
     flash[:notice] = t("controllers.notice.user_session_destroyed")
-    redirect_back_or_default root_path
+    redirect_to root_path
+    #redirect_back_or_default root_path
   end
 end
