dd9b7934cbf2f2541f28a63e241e022f98a8446c
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2010-03-05 15:09:57 +0100

    Revert "new bundler is loading init.rb from gems"
    This reverts commit b3bd5610b5a1db407d1dbcac7956a964ef964689.

---
+++ vendor/plugins/acts-as-taggable-on/init.rb
@@ -0,0 +1,3 @@
+ActiveRecord::Base.send :include, ActiveRecord::Acts::TaggableOn
+ActiveRecord::Base.send :include, ActiveRecord::Acts::Tagger
+ActionView::Base.send :include, TagsHelper if defined?(ActionView::Base)
---
+++ vendor/plugins/formtastic/init.rb
@@ -0,0 +1,2 @@
+# coding: utf-8
+ActionView::Base.send :include, Formtastic::SemanticFormHelper
